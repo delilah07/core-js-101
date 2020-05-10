4b5bcb444e8dbb357e98351842180d2e0232da4c
delilah07 <delilah07@github>
2020-05-10 22:09:12 +0200

    fix: Linter errors

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -23,7 +23,7 @@
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  Rectangle.prototype.getArea = function () {
+  this.getArea = function getArea() {
     return this.width * this.height;
   };
 }
